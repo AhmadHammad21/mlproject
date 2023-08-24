38e20954a03656141ad94927a8abbfbe6a9b7975
AhmadHammad21 <ahmedhammad8765@gmail.com>
2023-08-24 19:47:57 +0300

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,55 @@
+import os 
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_path_path: str = os.path.join('artifacts', 'train.csv')
+    test_path_path: str = os.path.join('artifacts', 'test.csv')
+    raw_path_path: str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self) -> None:
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        
+        # reading the data from local or any database
+        try:
+            df = pd.read_csv("data\stud.csv")
+            logging.info("Read the dataset as a dataframe")
+
+            # getting just the direftory name and creating a directory
+            os.makedirs(os.path.dirname(self.ingestion_config.train_path_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_path_path, index=False, header=True)
+            logging.info("Raw data has been saved")
+
+            logging.info("Train Test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_path_path, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_path_path, index=False, header=True)
+
+            logging.info("Train and Test data have been saved")
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_path_path,
+                self.ingestion_config.test_path_path, 
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+if __name__ == "__main__":
+    data_ingestion_object = DataIngestion()
+    data_ingestion_object.initiate_data_ingestion()
